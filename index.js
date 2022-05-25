d8ccf158e8a982eb60ab2aedc692c555d27ccfc6
Jubair <jubairhossen55@gmail.com>
2022-05-25 14:06:57 +0600

    add product api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-// const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,19 +25,29 @@ function verifyJwt(req, res, next) {
         next()
     });
 }
+// Database cunnection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ujpdfsf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// async function run() {
-//     try {
-//         await client.connect();
-//         const serviceCollection = client.db('doctors_portal').collection('services');
+async function run() {
+    try {
+        await client.connect();
+        const produtcsCollection = client.db('radon-electronics').collection('Products');
 
-//     }
-//     finally {
+        // add product api
+        app.post('/product', async (req, res) => {
+            const product = req.body;
+            const result = await produtcsCollection.insertOne(product);
+            res.send(result)
+        })
 
-//     }
-// }
+    }
+    finally {
 
-// run().catch(console.dir);
+    }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
